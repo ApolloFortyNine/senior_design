2bd243b1680bbbfbc7045de9c3d65e0ba5a2aaae
ajones98 <anthony.jones94@gmail.com>
2016-04-20 13:41:13 -0400

    Added files via upload

--- senior design chart/js/script.js
+++ senior design chart/js/script.js
@@ -1,28 +1,57 @@
 $(document).ready(function()
-{
-	var ctx = $("#myChart").get(0).getContext("2d");
+{	
+	// ========================================================
+	// Flex Sensors
+	// Bar Graph
+	// ========================================================
+		
+	var flexData = 
+	{
+		labels: ["Thumb", "Index", "Middle", "Ring", "Pinky"],
+		datasets: 
+		[
+			{
+				label: "Flex Sensors",
+				fillColor: "rgba(16,37,63,0.75)",
+				strokeColor: "rgba(16,37,63,0.75)",
+				highlightFill: "rgba(46,107,180,0.75)",
+				highlightStroke: "rgba(46,107,180,0.75)",
+				data: [0.36, 0.69, 0.80, 0.81, 0.56],
+			}
+		]
+	};
 	
-	var data = 
-	[
-		{
-			value: 270,
-			color: "cornflowerblue",
-			highlight:"lightskyblue",
-			label: "JavaScript"
-		},
-		{
-			value: 50,
-			color: "lightgreen",
-			highlight:"yellowgreen",
-			label: "HTML"
-		},
-		{
-			value: 40,
-			color: "orange",
-			highlight:"darkorange",
-			label: "CSS"
-		}	
-	];
+	// Get context of bar chart for flex sensors
+	var ctx = document.getElementById("flexBarChart").getContext("2d");
+
+	// Create the bar chart for the flex sensors
+	var flexChart = new Chart(ctx).Bar(flexData);
 	
-	var myNewChart = new Chart(ctx).Doughnut(data);
+	// ========================================================
+	// IMU Sensor
+	// Radar Graph
+	// ========================================================
+	
+	var imuData =
+	{
+		labels: ["Up", "Right", "Down", "Left"],
+		datasets:
+		[
+			{
+				label: "Flex Sensors",
+				fillColor: "rgba(16,37,63,0.75)",
+				strokeColor: "rgba(16,37,63,0.75)",
+				highlightFill: "rgba(46,107,180,0.75)",
+				highlightStroke: "rgba(46,107,180,0.75)",
+				data: [0.80, 0.49, 0.51, 0.23],
+				
+			}
+		]
+	};
+	
+	// Get context of radar chart for imu sensor
+	var ctx = document.getElementById("imuRadarChart").getContext("2d");
+	
+	// Create the radar chart for the flex sensor
+	var imuChart = new Chart(ctx).Radar(imuData);
 });
